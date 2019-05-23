87a189aadb19cd708a29d53c4255c719223c18c9
mike_b <mikeberadino@gmail.com>
2019-05-22 21:01:58 -0500

    Delete rev.py

--- rev.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image, ImageSequence
-
-
-im = Image.open('automata_gif.gif')
-frames = [frame.copy() for frame in ImageSequence.Iterator(im)]
-frames.reverse()
-
-frames[0].save('reversed.gif', save_all=True, append_images=frames[1:])
